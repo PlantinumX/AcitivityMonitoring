8df2bc9ba1c8a033a0e5cf3eff1149a89c37e5b3
Ömer Faruk Halici <halici@student.tugraz.at>
2019-04-26 16:41:27 +0200

    new approach with nn

---
+++ app/src/main/java/com/example/activitymonitoring/Matrix.java
@@ -0,0 +1,10 @@
+package com.example.activitymonitoring;
+
+import org.ejml.*;
+import org.ejml.simple.SimpleMatrix;
+
+//Stores the train data and
+public class AcitivityClassifier {
+	SimpleMatrix x_data = new SimpleMatrix(0,0);
+
+}
