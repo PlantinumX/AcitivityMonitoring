f306c55a4ede48f63edd44a6ff93e03236dbdf74
Ömer Faruk Halici <halici@student.tugraz.at>
2019-04-28 10:23:33 +0200

    changed some UI elements and made further test

--- app/src/main/java/com/example/activitymonitoring/Classifier.java
+++ app/src/main/java/com/example/activitymonitoring/Classifier.java
@@ -21,7 +21,7 @@ public class Classifier
 	private static final String INPUT_NODE = "input";
 	private static final String[] OUTPUT_NODES = {"y_"};
 	private static final String OUTPUT_NODE = "y_";
-	private static final long[] INPUT_SIZE = {1, 600};
+	private static final long[] INPUT_SIZE = {1, 270};
 	private static final int OUTPUT_SIZE = 6;
 	private static final String MODEL_FILE = "file:///android_asset/frozen_har.pb";
 	public Classifier(final Context context)
@@ -40,25 +40,13 @@ public class Classifier
         inferenceInterface.run(OUTPUT_NODES);
         inferenceInterface.fetch(OUTPUT_NODE, result);
         Log.e(MESSAGE_TAG,"RESULTS " + "Downstairs "+  Float.toString(result[0])+" Jogging " + Float.toString(result[1]) +" Sitting "+ Float.toString(result[2]) +" Standing " + Float.toString(result[3]) +"  Upstairs " +Float.toString(result[4]) + " Upstairs " +Float.toString(result[5]));
-		return result;
-//        double highest_probability = 0;
-//        int highest_probability_index = 0;
-//        for(int i = 0; i < result.length; i++)
-//        {
-//            if(highest_probability < result[i])
-//            {
-//                highest_probability = result[i];
-//                highest_probability_index = i;
-//            }
-//        }
-
-        // Walking= 5
-        // Jogging = 1
-        // Sitting = 2
-        // Standing = 3
-        // Upstairs	= 4
-        // Downstairs = 0
-//        return highest_probability_index;
+	    // Downstairs = 0
+	    // Jogging = 1
+	    // Sitting = 2
+	    // Standing = 3
+	    // Upstairs	= 4
+		// Walking= 5
+	    return result;
     }
 
 }
