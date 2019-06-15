e3aefac9a962131d0f3f0ad3e3c66ab907a15636
Halici Ömer <halici@student.tugraz.at>
2019-06-15 11:59:15 +0200

    outcommented particle filter

--- app/src/main/java/com/example/activitymonitoring/LocalizationActivity.java
+++ app/src/main/java/com/example/activitymonitoring/LocalizationActivity.java
@@ -150,7 +150,7 @@ public class LocalizationActivity extends BaseActivity implements SensorEventLis
             Log.d("distance", Double.toString(distance));
             Log.d("steps", Integer.toString(step_cnt));
             Toast.makeText(this, "duration: " + distance + "mean angle: " + Double.toString(mean_orientation), Toast.LENGTH_LONG).show();
-            particleFilter.moveParticles(distance,mean_orientation);
+//            particleFilter.moveParticles(distance,mean_orientation);
 
             motion.duration = (long)0;
             mean_orientation = 0;
